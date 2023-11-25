847c589672bce6b72caaa1b541e55a4589644944
PaoZin666 <150486415+PaoZin666@users.noreply.github.com>
2023-11-25 19:27:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-const menu = document.getElementById("menu");
-const projetos = document.getElementById("projetos");
-const github = document.getElementById("github");
-const temaEclipse = document.getElementById("tema-eclipse");
-const temaGrifo = document.getElementById("tema-grifo");
-temaGuts.volume = 0.4;
+const menu = document.getElementById('menu');
+const pj = document.getElementById('projetos');
+const gh = document.getElementById('github');
 
-function atualizarConteudo(videoSrc, personaSrc, descricaoHTML, gradientBackground) {
-    
-}
+function select(element) {
+    const links = document.querySelectorAll('.navitem');
+    links.forEach(link => {
+        link.classList.remove('ativo');
+    });
 
+    element.classList.add('ativo');
+    document.body.style.setProperty('--activeclr', element.dataset.clr);
 
+    const id = element.id;
+    const mainSection = document.querySelector('.Maim');
+    const gitSection = document.querySelector('.Git');
+    const iframeSection = document.querySelector('.iframes');
 
-botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
+    if (id === 'menu') {
+        menu.classList.add('active');
+		pj.classList.remove('active');
+		gh.classList.remove('active');
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
-});
+        mainSection.classList.add('ativo');
+        gitSection.classList.remove('ativo');
+        iframeSection.classList.remove('ativo');
+    } else if (id === 'github') {
+		menu.classList.remove('active');
+		pj.classList.remove('active');
+		gh.classList.add('active');
 
-function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
-}
+        mainSection.classList.remove('ativo');
+        gitSection.classList.add('ativo');
+        iframeSection.classList.remove('ativo');
+    } else if (id === 'projetos') {
+		menu.classList.remove('active');
+		pj.classList.add('active');
+		gh.classList.remove('active');
 
-function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+        mainSection.classList.remove('ativo');
+        gitSection.classList.remove('ativo');
+        iframeSection.classList.add('ativo');
+    }
 }
